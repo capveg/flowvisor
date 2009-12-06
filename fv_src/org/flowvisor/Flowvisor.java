b7a800b623d0c591918f954ca21bc7ab9b7752a5
Rob Sherwood <rob.sherwood@stanford.edu>
2009-12-05 19:13:49 -0800

    working build system

--- fv_src/org/flowvisor/Flowvisor.java
+++ fv_src/org/flowvisor/Flowvisor.java
@@ -3,10 +3,9 @@ package org.flowvisor;
 
 class Flowvisor
 {
-    public static int main(String args[])
+    public static void main(String args[])
     {
         for(int i=0; i< args.length; i++)
             System.out.println("Arg # " + i + " = " + args[i]);
-        return 0;
     }
 }
