29810f23da5aed91490223e92a2e52dcaf3e9e22
Rob Sherwood <rob.sherwood@stanford.edu>
2009-12-05 19:11:06 -0800

    renamed openflow files

---
+++ fv_src/org/flowvisor/Flowvisor.java
@@ -0,0 +1,12 @@
+
+package org.flowvisor;
+
+class Flowvisor
+{
+    public static int main(String args[])
+    {
+        for(int i=0; i< args.length; i++)
+            System.out.println("Arg # " + i + " = " + args[i]);
+        return 0;
+    }
+}
