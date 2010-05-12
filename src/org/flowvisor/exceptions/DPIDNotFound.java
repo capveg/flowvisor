827385ad1bfa409b55950ab8bbedf8fbd666238a
Rob Sherwood <rob.sherwood@stanford.edu>
2010-05-12 00:28:39 -0700

    forgot new exception file: added

---
+++ src/org/flowvisor/exceptions/DPIDNotFound.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package org.flowvisor.exceptions;
+
+/**
+ * @author capveg
+ *
+ */
+public class DPIDNotFound extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DPIDNotFound() {
+		super();
+	}
+
+	public DPIDNotFound(String arg0) {
+		super(arg0);
+	}
+	
+}
