b118a1659e061e4c7d64af7eb29c2f3c55b3f0d6
Rob Sherwood <rob.sherwood@stanford.edu>
2010-06-08 18:16:57 -0700

    code reformat

--- src/org/flowvisor/message/lldp/LLDPUtil.java
+++ src/org/flowvisor/message/lldp/LLDPUtil.java
@@ -14,27 +14,34 @@ import org.flowvisor.slicer.FVSlicer;
 
 /**
  * Set of utilities for handling our LLDP virtualization hacks
+ * 
  * @author capveg
- *
+ * 
  */
 public class LLDPUtil {
 	final public static short ETHER_LLDP = (short) 0x88cc;
 	final public static short ETHER_VLAN = (short) 0x8100;
+	final public static byte[] LLDP_MULTICAST = { 0x01, 0x23, 0x20, 0x00, 0x00,
+			0x01 };
+
 	/**
-	 * If this msg is lldp, then
-	 * 		  1) add a slice identifying trailer
-	 *        2) send to switch	  -- all slices can send lldp, no matter flowspace
-	 *        3) return true, we've handled this packet
-	 * else return false
-	 * @param po message
-	 * @param fvClassifier switch classifier
-	 * @param fvSlicer slice polcies
+	 * If this msg is lldp, then 1) add a slice identifying trailer 2) send to
+	 * switch -- all slices can send lldp, no matter flowspace 3) return true,
+	 * we've handled this packet else return false
+	 * 
+	 * @param po
+	 *            message
+	 * @param fvClassifier
+	 *            switch classifier
+	 * @param fvSlicer
+	 *            slice polcies
 	 * @return did we handle the message?
 	 */
-	static public boolean handleLLDPFromController(FVPacketOut po, FVClassifier fvClassifier, FVSlicer fvSlicer) {
-		if(!LLDPCheck(po.getPacketData()))
+	static public boolean handleLLDPFromController(FVPacketOut po,
+			FVClassifier fvClassifier, FVSlicer fvSlicer) {
+		if (!LLDPCheck(po.getPacketData()))
 			return false;
-		LLDPTrailer trailer = new LLDPTrailer(fvSlicer.getSliceName(),"fv1");
+		LLDPTrailer trailer = new LLDPTrailer(fvSlicer.getSliceName(), "fv1");
 		trailer.appendTo(po);
 		FVLog.log(LogLevel.DEBUG, fvSlicer, "applied lldp hack: " + po);
 		fvClassifier.getMsgStream().write(po);
@@ -43,14 +50,14 @@ public class LLDPUtil {
 
 	/**
 	 * Is this an lldp packet?
-	 *
+	 * 
 	 * @param po
 	 * @return
 	 */
 
 	static private boolean LLDPCheck(byte[] packetArray) {
-		if((packetArray == null) || (packetArray.length < 14))
-			return false;	// not lddp if no packet exists or too short
+		if ((packetArray == null) || (packetArray.length < 14))
+			return false; // not lddp if no packet exists or too short
 		ByteBuffer packet = ByteBuffer.wrap(packetArray);
 		short ether_type = packet.getShort(12);
 		if (ether_type == ETHER_VLAN)
@@ -60,26 +67,29 @@ public class LLDPUtil {
 		// TODO think about checking for NOX OID
 		return true;
 	}
+
 	/**
-	 * If this msg is lldp, then
-	 * 		1) remove the slice identifying trailer
-	 * 		2) send to controller  -- all slices can send lldp, no matter flowspace
-	 *      3) return true, we've handled this packet
+	 * If this msg is lldp, then 1) remove the slice identifying trailer 2) send
+	 * to controller -- all slices can send lldp, no matter flowspace 3) return
+	 * true, we've handled this packet
+	 * 
 	 * @param po
 	 * @param fvClassifier
 	 * @return did we handle this message?
 	 */
-	static public boolean handleLLDPFromSwitch(FVPacketIn pi, FVClassifier fvClassifier) {
-		if(!LLDPCheck(pi.getPacketData()))
+	static public boolean handleLLDPFromSwitch(FVPacketIn pi,
+			FVClassifier fvClassifier) {
+		if (!LLDPCheck(pi.getPacketData()))
 			return false;
 		LLDPTrailer trailer = LLDPTrailer.getTrailer(pi);
 		if (trailer == null)
 			return false;
-		FVSlicer fvSlicer = fvClassifier.getSlicerMap().get(trailer.getSliceName());
+		FVSlicer fvSlicer = fvClassifier.getSlicerMap().get(
+				trailer.getSliceName());
 		if (fvSlicer == null) {
-			FVLog.log(LogLevel.WARN, fvClassifier, "failed to undo llpd hack for unknown slice '" +
-					trailer.getSliceName() + "': " +
-					pi);
+			FVLog.log(LogLevel.WARN, fvClassifier,
+					"failed to undo llpd hack for unknown slice '"
+							+ trailer.getSliceName() + "': " + pi);
 			return false;
 		}
 		FVLog.log(LogLevel.DEBUG, fvSlicer, "undoing lldp hack: " + pi);
