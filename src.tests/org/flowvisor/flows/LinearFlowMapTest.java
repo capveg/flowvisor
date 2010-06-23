bf5239920a8923542f9a30b1104a5b5eedb4991a
Rob Sherwood <rob.sherwood@stanford.edu>
2010-06-23 13:31:45 -0700

    made FlowMap test more specific

--- src.tests/org/flowvisor/flows/LinearFlowMapTest.java
+++ src.tests/org/flowvisor/flows/LinearFlowMapTest.java
@@ -30,7 +30,7 @@ public class LinearFlowMapTest extends TestCase {
 				.fromString("OFMatch[in_port=1,dl_dst=00:1c:f0:ed:98:5a,dl_src=00:22:41:fa:73:01,dl_type=2048,dl_vlan=-1,dl_vpcp=0,nw_dst=3.4.4.5,nw_src=128.8.9.9,nw_proto=1,nw_tos=0,tp_dst=0,tp_src=8]");
 		List<FlowIntersect> intersects = submap.intersects(1, packet);
 		TestCase.assertEquals(1, intersects.size());
-		TestCase.assertTrue(intersects.get(0).getFlowEntry().matches(1, packet)
-				.getMatchType() != MatchType.NONE);
+		TestCase.assertEquals(MatchType.SUBSET, intersects.get(0)
+				.getFlowEntry().matches(1, packet).getMatchType());
 	}
 }
