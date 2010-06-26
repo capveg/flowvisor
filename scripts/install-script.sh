15cf3e61046de2d44692e6f93551dd4495128506
Rob Sherwood <rob.sherwood@stanford.edu>
2010-06-26 11:12:09 -0700

    fixed up install script: addressed #99, #100
    added commandline options to set everything
    included Josh's patch to set vars via environment
    added -r root option to install to a different
        directory base

--- scripts/install-script.sh
+++ scripts/install-script.sh
@@ -3,9 +3,11 @@
 # sadly hacked together install script; really only used to bootstrap rpm or debs
 # apologies in advance to anyone who has to debug this
 
-prefix=/usr/local
-fvuser=flowvisor
-fvgroup=flowvisor
+prefix_default=/usr/local
+fvuser_default=flowvisor
+fvgroup_default=flowvisor
+root_default=""
+
 install=install
 base=`dirname $0`/..
 scriptd=$base/scripts
@@ -14,13 +16,67 @@ dist=$base/dist
 config=flowvisor-config.xml
 #verbose=-v
 
+usage="$0 [-p prefix_dir] [-u flowvisor_user] [-g flowvisor_group] [-r root_dir]"
+usage_str="p:u:g:r:"
+
+while getopts $usage_str opt; do
+    case $opt in
+    p)
+        prefix=$OPTARG
+        echo "Set prefix to '$prefix'" >&2
+    ;;
+    u)
+        fvuser=$OPTARG
+        echo "Set fvuser to '$fvuser'" >&2
+    ;;
+    g)
+        fvgroup=$OPTARG
+        echo "Set fvgroup to '$fvgroup'" >&2
+    ;;
+    r)
+        root=$OPTARG
+        echo "Set root to '$root'" >&2
+    ;;
+    \?)
+        echo "Invalid option: -$OPTARG" >&2
+        cat << EOF  >&2
+        Usage:
+        $usage
+            defaults:
+                prefix_dir=$prefix_default
+                fvuser=$fvuser_default
+                fvgroup=$fvgroup_default
+                root=$root_default
+EOF
+        exit 1
+    ;;
+esac
+done
+
 echo "Using source dir: $base"
-read -p "Installation prefix ($prefix): " prefix
-read -p "FlowVisor User ($fvuser): " fvuser
-read -p "FlowVisor Group ($fvgroup): " fvgroup
+
+test -z "$prefix" && read -p "Installation prefix ($prefix_default): " prefix
+if [ "X$prefix" == "X" ] ; then
+    prefix=$prefix_default
+fi
+
+test -z "$fvuser" && read -p "FlowVisor User ($fvuser_default): " fvuser
+if [ "X$fvuser" == "X" ] ; then
+    fvuser=$fvuser_default
+fi
+
+test -z "$fvgroup" && read -p "FlowVisor Group ($fvgroup_default): " fvgroup
+if [ "X$fvgroup" == "X" ] ; then
+    fvgroup=$group_default
+fi
+
+test -z "$root" && read -p "Install to different root directory ($root_default) " root
+if [ "X$root" == "X" ] ; then
+    root=$root_default
+fi
 
 
-echo Installing FlowVisor into $prefix as user/group ${fvuser}:${fvgroup}
+echo Installing FlowVisor into $root$prefix with prefix=$prefix as user/group ${fvuser}:${fvgroup}
 
 bin_SCRIPTS="\
     fvctl \
@@ -53,26 +109,26 @@ done
 echo Creating directories
 for d in bin sbin libexec/flowvisor etc/flowvisor ; do 
     echo Creating $prefix/$d
-    $install $verbose --owner=$fvuser --group=$fvgroup --mode=755 -d $prefix/$d
+    $install $verbose --owner=$fvuser --group=$fvgroup --mode=755 -d $root$prefix/$d
 done
 
 echo Installing scripts
-$install $verbose --owner=$fvuser --group=$fvgroup --mode=755 -D $bin_SCRIPTS $prefix/bin
-$install $verbose --owner=$fvuser --group=$fvgroup --mode=755 -D $sbin_SCRIPTS $prefix/sbin
+$install $verbose --owner=$fvuser --group=$fvgroup --mode=755 -D $bin_SCRIPTS $root$prefix/bin
+$install $verbose --owner=$fvuser --group=$fvgroup --mode=755 -D $sbin_SCRIPTS $root$prefix/sbin
 
 
 echo Installing jars
 cd $owd
 cd $libs
-$install $verbose --owner=$fvuser --group=$fvgroup --mode=644 -D $LIBS $prefix/libexec/flowvisor/.
+$install $verbose --owner=$fvuser --group=$fvgroup --mode=644 -D $LIBS $root$prefix/libexec/flowvisor/.
 
 echo Installing flowvisor.jar
 cd $owd
 cd $dist
-$install $verbose --owner=$fvuser --group=$fvgroup --mode=644 -D flowvisor.jar  $prefix/libexec/flowvisor/.
+$install $verbose --owner=$fvuser --group=$fvgroup --mode=644 -D flowvisor.jar  $root$prefix/libexec/flowvisor/.
 
 echo Installing configs
 cd $owd
-$install $verbose --owner=$fvuser --group=$fvgroup --mode=644 $scriptd/envs $prefix/etc/flowvisor/envs.sh
-$install $verbose --owner=$fvuser --group=$fvgroup --mode=644 $base/mySSLKeyStore $prefix/etc/flowvisor
-$prefix/sbin/config-gen-default $prefix/etc/flowvisor/$config
+$install $verbose --owner=$fvuser --group=$fvgroup --mode=644 $scriptd/envs $root$prefix/etc/flowvisor/envs.sh
+$install $verbose --owner=$fvuser --group=$fvgroup --mode=644 $base/mySSLKeyStore $root$prefix/etc/flowvisor
+$prefix/sbin/config-gen-default $root$prefix/etc/flowvisor/$config
