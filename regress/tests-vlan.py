997f03708c85f4116b19ca21789c86308506e7e1
Rob Sherwood <rob.sherwood@stanford.edu>
2010-07-07 10:46:43 -0700

    extended test case for #124
    Added explicit packets to match on vlans
    and to not match on vlans and everything
    still seems to work /shrug

--- regress/tests-vlan.py
+++ regress/tests-vlan.py
@@ -48,11 +48,31 @@ try:
             c0a8 0028 c0a8 0128 7a18 586b 1108 97f5
             19e2 657e 07cc 31c3 11c7 c40c 8b95 5151
             3354 51d5 0036'''
+    packet_vlan1 =  FvRegress.OFVERSION + '''0a 0056 0000 0000 0000 0101
+            0040 0003 0000 0000 0000 0001 0000 0000
+            0002 8100       
+            0001 0800            
+            4500 0032 0000 0000 40ff f72c
+            c0a8 0028 c0a8 0128 7a18 586b 1108 97f5
+            19e2 657e 07cc 31c3 11c7 c40c 8b95 5151
+            3354 51d5 0036'''
+    packet_vlan812 =  FvRegress.OFVERSION + '''0a 0056 0000 0002 0000 0101
+            0040 0003 0000 0000 0000 0002 0000 0000
+            0001 8100
+            032c 0800 
+            4500 0032 0000 0000 40ff f72c
+            c0a8 0028 c0a8 0128 7a18 586b 1108 97f5
+            19e2 657e 07cc 31c3 11c7 c40c 8b95 5151
+            3354 51d5 0036'''
     h.runTest(name="switch2controller packet_in routing - shared port", timeout=timeout, events= [
             TestEvent( "send","switch",'switch1', packet_no_vlan1),
             TestEvent( "recv","guest",'bob', packet_no_vlan1),
             TestEvent( "send","switch",'switch1', packet_no_vlan2),
             TestEvent( "recv","guest",'bob', packet_no_vlan2),
+            TestEvent( "send","switch",'switch1', packet_vlan1),
+            TestEvent( "recv","guest",'bob', packet_vlan1),
+            TestEvent( "send","switch",'switch1', packet_vlan812),
+            TestEvent( "recv","guest",'alice', packet_vlan812),
             ])
 
 #########################################
