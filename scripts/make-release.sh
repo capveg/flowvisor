a64c51f15bda29a5d6c85c47996e9fbc1bcf4bb8
Rob Sherwood <rob.sherwood@stanford.edu>
2010-07-17 11:22:39 -0700

    Release flowvisor-0.6-alpha-GPO-12
    now with working make release script

--- scripts/make-release.sh
+++ scripts/make-release.sh
@@ -8,6 +8,18 @@ if [ "X$1" = "X" ] ; then
 fi
 
 release=$1
-sed -i "s/FLOWVISOR_VERSION = \"([^\"])\"/FLOWVISOR_VERSION = \"$release\"/" $fv_main
-git diff
+sed -i "s/FLOWVISOR_VERSION = [^ ]\+/FLOWVISOR_VERSION = \"$release\";/" $fv_main
+git diff $fv_main
+
+read -p "Are these changes what you wanted? 'y' to continue, anything else to abort" res
+
+if [ "X$res" != "Xy" ] ; then
+    echo "Aborting; resolve by hand..." >&2
+    exit 2
+fi
+
+git commit -m "Release $release" $fv_main
+git tag $release
+
+echo "If you're happy, do 'git push --tags'" >&2
 
