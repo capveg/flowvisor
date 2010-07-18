4b7929d6b0e4ba615ce80ac784b069f970a17476
Rob Sherwood <rob.sherwood@stanford.edu>
2010-07-18 16:42:27 -0700

    updated make-release script
    force you to run `make tests`

--- scripts/make-release.sh
+++ scripts/make-release.sh
@@ -11,11 +11,17 @@ release=$1
 sed -i "s/FLOWVISOR_VERSION = [^ ]\+/FLOWVISOR_VERSION = \"$release\";/" $fv_main
 git diff $fv_main
 
-read -p "Are these changes what you wanted? 'y' to continue, anything else to abort" res
-
+read -p "Are these changes what you wanted? 'y' to continue, anything else to abort " res
 if [ "X$res" != "Xy" ] ; then
     echo "Aborting; resolve by hand..." >&2
     exit 2
+fi
+
+
+make tests
+if [ $? != 0 ] ;
+    echo "make tests seem to have failed... aborting release" >&2
+    exit 1
 fi
 
 git commit -m "Release $release" $fv_main
