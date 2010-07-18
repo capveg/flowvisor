7a3bc9bcbb14da1ea9af6ef3ea7d81af4220f38c
Rob Sherwood <rob.sherwood@stanford.edu>
2010-07-17 19:53:23 -0700

    new lldp tests

--- regress/tests-kk-topo.py
+++ regress/tests-kk-topo.py
@@ -34,58 +34,59 @@ try:
     if wantPause:
         doPause("start tests")
     ################################################################
-    lldp_out =     FvRegress.OFVERSION + '''0d 0058 0000 abcd ffff ffff
-                ffff 0008 0000 0008 0001 0080 0123 2000
-                0001 0000 0000 0000 88cc 4500 0032 0000
-                4000 4011 2868 c0a8 c800 c0a8 c901 0001
-                0000 001e d7c3 cdc0 251b e6dc ea0c 726d
-                983f 2b71 c2e4 1b6f bc11 8250'''
+    lldp_out = FvRegress.OFVERSION + '''0d 00 3a 00 00 00 00 ff ff ff ff ff fd 00 08
+                00 00 00 08 00 01 00 00 01 23 20 00 00 01 00 12
+                e2 b8 dc 4c 88 cc 02 07 04 e2 b8 dc 3b 17 95 04
+                03 02 00 01 06 02 00 78 00 00'''
+    lldp_out_after = FvRegress.OFVERSION + '''0d 00 5d 00 00 00 00 ff ff ff ff ff fd 00 08
+                00 00 00 08 00 01 00 00 01 23 20 00 00 01 00 12
+                e2 b8 dc 4c 88 cc 02 07 04 e2 b8 dc 3b 17 95 04
+                03 02 00 01 06 02 00 78 00 00 02 23 07 70 72 6f
+                64 00 20 20 20 20 6d 61 67 69 63 20 66 6c 6f 77
+                76 69 73 6f 72 31 00 05 15 de ad ca fe'''
+    h.runTest(name="lldp hack", timeout=timeout, events= [
+            TestEvent( "send","guest",'prod', lldp_out),
+            TestEvent( "recv","switch",'switch1', lldp_out_after),
+            ])
+    ################################################################
+
+    # 34 bytes
+    probe = '''01 23 20 00 00 01 00 12 e2 b8 dc 4c 88 cc 02 07
+               04 e2 b8 dc 3b 17 95 04 03 02 00 01 06 02 00 78
+               00 00'''
+    # 35 bytes
+    trailer_prod = \
+            '''02 23 07 70 72 6f 64 00 20 20 20 20 6d 61 67 69 
+               63 20 66 6c 6f 77 76 69 73 6f 72 31 00 05 15 de 
+               ad ca fe'''
+    # 36 bytes
+    trailer_ncast =  \
+            ''' 02 24 07 6e 63 61 73 74 00 20 20 20 20 6d 61 67 
+                69 63 20 66 6c 6f 77 76 69 73 6f 72 31 00 06 15 
+                de ad ca fe'''
+    # 24 + probe
+    lldp_out =     FvRegress.OFVERSION + '''0d 003a 0000 abcd ffff ffff
+                ffff 0008 0000 0008 0001 0080''' + probe
+    # 24 + probe + trailer = 93 0x5d
     lldp_out_after_prod = FvRegress.OFVERSION + \
-                '''0d 00 7b 00 00 ab cd ff ff ff ff ff ff 00 08
-                00 00 00 08 00 01 00 80 01 23 20 00 00 01 00 00
-                00 00 00 00 88 cc 45 00 00 32 00 00 40 00 40 11
-                28 68 c0 a8 c8 00 c0 a8 c9 01 00 01 00 00 00 1e
-                d7 c3 cd c0 25 1b e6 dc ea 0c 72 6d 98 3f 2b 71
-                c2 e4 1b 6f bc 11 82 50 11 81 07 70 72 6f 64 00
-                20 20 20 20 6d 61 67 69 63 20 66 6c 6f 77 76 69
-                73 6f 72 31 00 05 15 de ad ca fe'''
+                '''0d 00 5d 00 00 ab cd ff ff ff ff ff ff 00 08
+                00 00 00 08 00 01 00 80''' + probe + trailer_prod
+    # 24 + probe + trailer = 94 0x5e
     lldp_out_after_ncast = FvRegress.OFVERSION + \
-                '''0d 00 7c 00 00 ab cd ff ff ff ff ff ff 00 08
-                00 00 00 08 00 01 00 80 01 23 20 00 00 01 00 00
-                00 00 00 00 88 cc 45 00 00 32 00 00 40 00 40 11
-                28 68 c0 a8 c8 00 c0 a8 c9 01 00 01 00 00 00 1e
-                d7 c3 cd c0 25 1b e6 dc ea 0c 72 6d 98 3f 2b 71
-                c2 e4 1b 6f bc 11 82 50 12 01 07 6e 63 61 73 74
-                00 20 20 20 20 6d 61 67 69 63 20 66 6c 6f 77 76
-                69 73 6f 72 31 00 06 15 de ad ca fe'''
+                '''0d 00 5e 00 00 ab cd ff ff ff ff ff ff 00 08
+                00 00 00 08 00 01 00 80''' + probe + trailer_ncast
+    # 18 + probe + trailer = 87 0x57
     lldp_in_prod =      FvRegress.OFVERSION + \
-                    '''0a 00 64 00 00 00 00 00 00 01 01
-                    00 40 00 03 00 00 01 23 20 00 00 01 00 00
-                    00 00 00 00 88 cc 45 00 00 32 00 00 40 00 40 11
-                    28 68 c0 a8 c8 00 c0 a8 c9 01 00 01 00 00 00 1e
-                    d7 c3 cd c0 25 1b e6 dc ea 0c 72 6d 98 3f 2b 71
-                    c2 e4 1b 6f bc 11 82 50 08 81 07 
-                    70 72 6f 64 00
-                    66 76 31 00 
-                    05 04 de ad ca fe'''
+                '''0a 00 57 00 00 00 00 00 00 01 01 00 45 00 03 
+                00 00''' + probe + trailer_prod
+    # 18 + probe + trailer = 88 0x59
     lldp_in_ncast =      FvRegress.OFVERSION + \
-                    '''0a 00 65 00 00 00 00 00 00 01 01
-                    00 40 00 03 00 00 01 23 20 00 00 01 00 00
-                    00 00 00 00 88 cc 45 00 00 32 00 00 40 00 40 11
-                    28 68 c0 a8 c8 00 c0 a8 c9 01 00 01 00 00 00 1e
-                    d7 c3 cd c0 25 1b e6 dc ea 0c 72 6d 98 3f 2b 71
-                    c2 e4 1b 6f bc 11 82 50 08 81 07 
-                    6e 63 61 73 74 00
-                    66 76 31 00 
-                    06 04 de ad ca fe'''
-                    
+                '''0a 00 58 00 00 00 00 00 00 01 01 00 46 00 03 
+                00 00''' + probe + trailer_ncast 
+    # 18 + probe = 52 0x34                ; 
     lldp_in_after =  FvRegress.OFVERSION + \
-                '''0a 00 52 00 00 00 00 00 00 01 01 00 40 00 03
-                00 00 01 23 20 00 00 01 00 00 00 00 00 00 88 cc
-                45 00 00 32 00 00 40 00 40 11 28 68 c0 a8 c8 00
-                c0 a8 c9 01 00 01 00 00 00 1e d7 c3 cd c0 25 1b
-                e6 dc ea 0c 72 6d 98 3f 2b 71 c2 e4 1b 6f bc 11
-                82 50'''
+                '''0a 00 34 00 00 00 00 00 00 01 01 00 22 00 03
+                00 00''' + probe
     h.runTest(name="lldp hack", timeout=timeout, events= [
             TestEvent( "send","guest",'prod', lldp_out),
             TestEvent( "recv","switch",'switch1', lldp_out_after_prod),
@@ -106,6 +107,33 @@ try:
             TestEvent( "send","switch",'switch1', arp_in),
             TestEvent( "recv","guest",'prod', arp_in),
             ])
+#########################################
+# multi-layer lldp
+    # 24 + probe + trailer*2 = 128 0x80
+    lldp_out_after_prod_prod = FvRegress.OFVERSION + \
+                '''0d 00 80 00 00 ab cd ff ff ff ff ff ff 00 08
+                00 00 00 08 00 01 00 80''' + probe + trailer_prod + trailer_prod
+    # 18 + probe + trailer*2 = 81 0x7a
+    lldp_in_prod_prod =      FvRegress.OFVERSION + \
+                '''0a 00 7a 00 00 00 00 00 00 01 01 00 45 00 03 
+                00 00''' + probe + trailer_prod + trailer_prod
+    h.runTest(name="recursive lldp hack", timeout=timeout, events= [
+            # prod sends lldp
+            TestEvent( "send","guest",'prod', lldp_out),
+            # switch recvs
+            TestEvent( "recv","switch",'switch1', lldp_out_after_prod),
+            # prod sends into switch again
+            TestEvent( "send","guest",'prod', lldp_out_after_prod),
+            # switch recvs again
+            TestEvent( "recv","switch",'switch1', lldp_out_after_prod_prod),
+            # lldp comes back up first FV
+            TestEvent( "send","switch",'switch1', lldp_in_prod_prod),
+            TestEvent( "recv","guest",'prod', lldp_in_prod),
+            # lldp comes back up through 2nd FV
+            TestEvent( "send","switch",'switch1', lldp_in_prod),
+            TestEvent( "recv","guest",'prod', lldp_in_after),
+            ])
+    
 #########################################
 # more tests for this setup HERE
 #################################### End Tests
