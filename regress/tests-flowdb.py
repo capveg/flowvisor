4bf62328fa1f12e2d61d6d0f7d60076e6bfb5ff8
Rob Sherwood <rob.sherwood@stanford.edu>
2011-02-11 14:32:26 -0800

    store the original actions list in flowRewriteDB
    bugfix: the "original" in the flowRewriteDB was actually
    post-actions being rewritten.. Fixed.
    
    Very hard to test given current XMLRPC + API :-(

--- regress/tests-flowdb.py
+++ regress/tests-flowdb.py
@@ -66,30 +66,46 @@ try:
                           00 00 00 02 33 0c 29 c6 36 8d ff ff 00 00 08 06
                           00 02 00 00 c0 01 f9 7b c0 01 f9 79 00 00 00 00
                           00 00 00 00 00 00 00 00 00 00 00 05 00 00 80 00
-                          00 00 01 6f 00 00 00 01 00 00 00 08 00 02 00 00'''
+                          00 00 01 6f 00 00 00 01 00 00 00 08 ff fc 00 00'''
+    flow_mod3_after = FvRegress.OFVERSION + \
+                    '''0e 00 58 00 00 01 04 00 00 00 00 00 00 00 00
+                    00 00 00 02 33 0c 29 c6 36 8d ff ff 00 00 08 06
+                    00 02 00 00 c0 01 f9 7b c0 01 f9 79 00 00 00 00
+                    00 00 00 00 00 00 00 00 00 00 00 05 00 00 80 00
+                    00 00 01 6f 00 00 00 01 00 00 00 08 00 02 00 00
+                    00 00 00 08 00 03 00 00'''
     h.runTest(name="flow_mod install",timeout=timeout,  events= [
           # send flow_mod, make sure it succeeds
           TestEvent( "send","guest",'alice', flow_mod1),
           TestEvent( "recv","switch",'switch1', flow_mod1), 
           TestEvent( "send","guest",'alice', flow_mod2),
           TestEvent( "recv","switch",'switch1', flow_mod2), 
+          # this test sends an OFPP_ALL and make sures it expands
+          # FIXME: fix API so we can test if the expand worked...
+          # right now, this test is half-assed
           TestEvent( "send","guest",'alice', flow_mod3),
-          TestEvent( "recv","switch",'switch1', flow_mod3), 
+          TestEvent( "recv","switch",'switch1', flow_mod3_after), 
           ])
 
 ####################################
+    print "Testing getSwitchFlowDB"            
     flows = s.api.getSwitchFlowDB("1")
     if len(flows) != 3:
         test_failed("Wanted 3 flows, got %d" % len(flows))
+    else: 
+        print "     SUCCESS"
     print "Got %d flows" % len(flows)
     for flow in flows:
         print "==== Got flow "
         for key,val in flow.iteritems():
             print "     %s=%s" % (key,val)
 ####################################
+    print "Testing getSliceRewriteDB"            
     rewriteDB = s.api.getSliceRewriteDB("alice","1")
     if len(rewriteDB) != 3:
         test_failed("Wanted 3 rewrites, got %d" % len(rewriteDB))
+    else: 
+        print "     SUCCESS"
     for original, rewrites in rewriteDB.iteritems():
         print "========= Original: '%s'" % original
         print "========= " 
@@ -99,6 +115,15 @@ try:
                 print "     => %s=%s" % (key,val)
     #print "Sleeping!"
     #time.sleep(100000)
+
+####################################
+    flow_expire = FvRegress.OFVERSION + '''0b 00 58 00 00 00 01 00 3f ff ff 00 00 00 00
+                00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+                00 00 00 00 00 00 00 00 00 00 00 00 80 00 01 00
+                00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+                00 00 00 0b 00 00 00 00 00 00 00 00 00 00 01 e1
+                00 00 00 00 00 00 02 58
+                '''
 
 #########################################
 # more tests for this setup HERE
--- src/org/flowvisor/message/FVFlowMod.java
+++ src/org/flowvisor/message/FVFlowMod.java
@@ -11,6 +11,7 @@ import org.flowvisor.log.LogLevel;
 import org.flowvisor.slicer.FVSlicer;
 import org.openflow.protocol.OFError.OFBadActionCode;
 import org.openflow.protocol.OFError.OFFlowModFailedCode;
+import org.openflow.protocol.OFFlowMod;
 import org.openflow.protocol.action.OFAction;
 
 public class FVFlowMod extends org.openflow.protocol.OFFlowMod implements
@@ -50,18 +51,21 @@ public class FVFlowMod extends org.openflow.protocol.OFFlowMod implements
 					OFBadActionCode.OFPBAC_EPERM, this), fvSlicer);
 			return;
 		}
-		int oldALen = FVMessageUtil.countActionsLen(this.getActions());
-		this.setActions(actionsList);
-		// set new length as a function of old length and old actions length
-		this.setLength((short) (getLength() - oldALen + FVMessageUtil
-				.countActionsLen(actionsList)));
 		// expand this match to everything that intersects the flowspace
 		List<FlowIntersect> intersections = fvSlicer.getFlowSpace().intersects(
 				fvClassifier.getDPID(), this.match);
 
 		int expansions = 0;
-		for (FlowIntersect intersect : intersections) {
-			try {
+		try {
+			OFFlowMod original = this.clone(); // keep an unmodified copy
+			int oldALen = FVMessageUtil.countActionsLen(this.getActions());
+			this.setActions(actionsList);
+			// set new length as a function of old length and old actions length
+			this.setLength((short) (getLength() - oldALen + FVMessageUtil
+					.countActionsLen(actionsList)));
+
+			for (FlowIntersect intersect : intersections) {
+
 				if (intersect.getFlowEntry().hasPermissions(
 						fvSlicer.getSliceName(), SliceAction.WRITE)) {
 					expansions++;
@@ -69,19 +73,20 @@ public class FVFlowMod extends org.openflow.protocol.OFFlowMod implements
 					// replace match with the intersection
 					newFlowMod.setMatch(intersect.getMatch());
 					// update flowDBs
-					fvSlicer.getFlowRewriteDB().processFlowMods(this,
+					fvSlicer.getFlowRewriteDB().processFlowMods(original,
 							newFlowMod);
 					fvClassifier.getFlowDB().processFlowMod(newFlowMod,
 							fvClassifier.getDPID(), fvSlicer.getSliceName());
 					// actually send msg
 					fvClassifier.sendMsg(newFlowMod, fvSlicer);
 				}
-			} catch (CloneNotSupportedException e) {
-				FVLog.log(LogLevel.CRIT, fvSlicer,
-						"FlowMod does not implement clone()!?: ", e);
-				return;
 			}
+		} catch (CloneNotSupportedException e) {
+			FVLog.log(LogLevel.CRIT, fvSlicer,
+					"FlowMod does not implement clone()!?: ", e);
+			return;
 		}
+
 		if (expansions == 0) {
 			FVLog.log(LogLevel.WARN, fvSlicer, "dropping illegal fm: ", this);
 			fvSlicer.sendMsg(FVMessageUtil.makeErrorMsg(
