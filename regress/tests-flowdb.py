fe4b4d0ba18f886f6f27f6249dc2b2351aef3f84
Rob Sherwood <rob.sherwood@stanford.edu>
2011-02-13 15:40:46 -0800

    create test to dynamically change flow_tracking
    correctly doesn't pass; need to implement

--- regress/tests-flowdb.py
+++ regress/tests-flowdb.py
@@ -88,6 +88,66 @@ try:
           ])
 
 ####################################
+    print "Testing getSwitchFlowDB (track_flows disabled)"            
+    flows = s.api.getSwitchFlowDB("1")
+    if len(flows) != 0:
+        test_failed("Wanted 0 flows, got %d" % len(flows))
+    else: 
+        print "     SUCCESS"
+    print "Got %d flows" % len(flows)
+    for flow in flows:
+        print "==== Got flow "
+        for key,val in flow.iteritems():
+            print "     %s=%s" % (key,val)
+#################################### Start Tests
+    print "Testing setConfig track_flows=True"
+    try:
+        if not s.api.setConfig("flowvisor!track_flows","True"): 
+            test_failed("setConfig returned False")
+        else:
+            print "     SUCCESS"
+    except StandardError,e :
+            test_failed("setConfig returned: %s" % str(e))
+#################################### Start Tests
+    flow_mod1 = FvRegress.OFVERSION + \
+                          '''0e 00 50 40 00 90 b6 00 00 00 00 00 00 00 00
+                          00 00 00 02 00 0c 29 c6 36 8d ff ff 00 00 08 06
+                          00 02 00 00 c0 01 f9 7b c0 01 f9 79 00 00 00 00
+                          00 00 00 00 00 00 00 00 00 00 00 05 00 00 80 00
+                          00 00 01 6f 00 00 00 01 00 00 00 08 00 02 00 00'''
+    flow_mod2 = FvRegress.OFVERSION + \
+                          '''0e 00 50 40 00 90 b6 00 00 00 00 00 00 00 00
+                          00 00 00 02 22 0c 29 c6 36 8d ff ff 00 00 08 06
+                          00 02 00 00 c0 01 f9 7b c0 01 f9 79 00 00 00 00
+                          00 00 00 00 00 00 00 00 00 00 00 05 00 00 80 00
+                          00 00 01 6f 00 00 00 01 00 00 00 08 00 02 00 00'''
+    flow_mod3 = FvRegress.OFVERSION + \
+                          '''0e 00 50 40 00 90 b6 00 00 00 00 00 00 00 00
+                          00 00 00 02 33 0c 29 c6 36 8d ff ff 00 00 08 06
+                          00 02 00 00 c0 01 f9 7b c0 01 f9 79 00 00 00 00
+                          00 00 00 00 00 00 00 00 00 00 00 05 00 00 80 00
+                          00 00 01 6f 00 00 00 01 00 00 00 08 ff fc 00 00'''
+    flow_mod3_after = FvRegress.OFVERSION + \
+                    '''0e 00 58 00 00 01 04 00 00 00 00 00 00 00 00
+                    00 00 00 02 33 0c 29 c6 36 8d ff ff 00 00 08 06
+                    00 02 00 00 c0 01 f9 7b c0 01 f9 79 00 00 00 00
+                    00 00 00 00 00 00 00 00 00 00 00 05 00 00 80 00
+                    00 00 01 6f 00 00 00 01 00 00 00 08 00 02 00 00
+                    00 00 00 08 00 03 00 00'''
+    h.runTest(name="flowdb install",timeout=timeout,  events= [
+          # send flow_mod, make sure it succeeds
+          TestEvent( "send","guest",'alice', flow_mod1),
+          TestEvent( "recv","switch",'switch1', flow_mod1), 
+          TestEvent( "send","guest",'alice', flow_mod2),
+          TestEvent( "recv","switch",'switch1', flow_mod2), 
+          # this test sends an OFPP_ALL and make sures it expands
+          # FIXME: fix API so we can test if the expand worked...
+          # right now, this test is half-assed
+          TestEvent( "send","guest",'alice', flow_mod3),
+          TestEvent( "recv","switch",'switch1', flow_mod3_after), 
+          ])
+
+####################################
     print "Testing getSwitchFlowDB"            
     flows = s.api.getSwitchFlowDB("1")
     if len(flows) != 3:
