71b85a23d5ca75cb640b9426f618585a00eb4c77
Rob Sherwood <rob.sherwood@stanford.edu>
2011-02-22 13:57:13 -0800

    Release flowvisor-0.7.1

--- src/org/flowvisor/FlowVisor.java
+++ src/org/flowvisor/FlowVisor.java
@@ -32,7 +32,7 @@ public class FlowVisor {
 	public final static int FLOWVISOR_VENDOR_EXTENSION = 0x80000001;
 
 	// VERSION
-	public final static String FLOWVISOR_VERSION = "flowvisor-0.7.0";
+	public final static String FLOWVISOR_VERSION = "flowvisor-0.7.1";
 
 	// Max slicename len ; used in LLDP for now; needs to be 1 byte
 	public final static int MAX_SLICENAME_LEN = 255;
