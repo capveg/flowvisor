05e292b48f03c2d673a4e741fa5b3b6a901c9246
Rob Sherwood <rob.sherwood@stanford.edu>
2011-05-16 15:34:33 -0700

    Revert "hack to make vendor stats extensions pass through FV correctly"
    this was an OpenFLowj problem and this fix didn't work anyway.
    
    This reverts commit 43a49e739817b951ef2ab8b6d8c232cc6ab8cede.

--- src/org/flowvisor/message/statistics/FVVendorStatistics.java
+++ src/org/flowvisor/message/statistics/FVVendorStatistics.java
@@ -1,7 +1,5 @@
 package org.flowvisor.message.statistics;
 
-import java.nio.ByteBuffer;
-
 import org.flowvisor.classifier.FVClassifier;
 import org.flowvisor.message.FVMessageUtil;
 import org.flowvisor.slicer.FVSlicer;
@@ -10,23 +8,6 @@ import org.openflow.protocol.statistics.OFVendorStatistics;
 
 public class FVVendorStatistics extends OFVendorStatistics implements
 		SlicableStatistic, ClassifiableStatistic {
-
-	OFMessage vendor; // weird protocol thing; a vendor_stats message has an
-
-	// openflow message header followed by a body
-
-	@Override
-	public void readFrom(ByteBuffer data) {
-		vendor = new OFMessage();
-		// this should read the header AND body of the stat
-		vendor.readFrom(data);
-	}
-
-	@Override
-	public void writeTo(ByteBuffer data) {
-		if (vendor != null)
-			vendor.writeTo(data);
-	}
 
 	@Override
 	public void sliceFromController(OFMessage msg, FVClassifier fvClassifier,
@@ -41,6 +22,7 @@ public class FVVendorStatistics extends OFVendorStatistics implements
 
 	@Override
 	public int getLength() {
-		return vendor.getLengthU();
+		// TODO Auto-generated method stub :: ????
+		return 0;
 	}
 }
